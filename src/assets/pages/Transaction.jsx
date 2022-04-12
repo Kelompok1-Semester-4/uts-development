e98a078b5e5206aecba870c3b97a8b48c33c8390
E41200415NOGA <nogamuktiwati@gmail.com>
2022-04-12 12:38:35 +0700

    search courses

--- src/assets/pages/Transaction.jsx
+++ src/assets/pages/Transaction.jsx
@@ -11,87 +11,117 @@ import image_transaction from "../../assets/images/image_transaction.svg";
 import icon_info from "../../assets/images/icon_info.svg";
 
 const Transaction = () => {
+  let { id } = useParams();
+  const [course, setCourse] = useState({});
 
-    let { id } = useParams();
-    const [course, setCourse] = useState({});
+  useEffect(() => {
+    axios
+      .get(`http://127.0.0.1:8000/api/courses?id=${id}`)
+      .then((res) => {
+        console.log(res.data);
+        setCourse(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [id]);
 
-    useEffect(() => {
-        axios.get(`http://127.0.0.1:8000/api/courses?id=${id}`)
-            .then(res => {
-                console.log(res.data);
-                setCourse(res.data);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }, [id]);
+  return (
+    <div>
+      <Header />
+      <div className="container transaction">
+        <div className="row d-flex align-items-center">
+          <div className="col-md-6">
+            <h5>Transaction Form</h5>
+            <h2>{course.title}</h2>
 
-    return (
-        <div>
-            <Header />
-            <div className="container transaction">
-                <div className="row d-flex align-items-center">
-                    <div className="col-md-6">
-                        <h5>Transaction Form</h5>
-                        <h2>{course.title}</h2>
+            <img
+              src={image_transaction}
+              className="transaction-image mt-5"
+              alt=""
+            />
+          </div>
+          <div className="col-md-6">
+            <p className="text-secondary">
+              <b className="text-black">Profile</b> • Please fill in your data
+              correctl{" "}
+            </p>
 
-                        <img src={image_transaction} className="transaction-image mt-5" alt="" />
-                    </div>
-                    <div className="col-md-6">
-                        <p className="text-secondary"><b className="text-black">Profile</b> •  Please fill in your data correctl </p>
-
-                        <div className="row">
-                            <form className="form-register">
-                                <div className="row">
-                                    <div className="col">
-                                        <input type="text" className="form-control" id="fullname" placeholder="Fullname" />
-                                    </div>
-                                    <div className="col">
-                                        <input type="text" className="form-control" id="phone-number" placeholder="Phone Number" />
-                                    </div>
-                                </div>
-                                <div className="row mt-4">
-                                    <div className="col">
-                                        <select id="inputState" className="form-select">
-                                            <option selected>Gender</option>
-                                            <option>L</option>
-                                            <option>P</option>
-                                        </select>
-                                    </div>
-                                    <div className="col">
-                                        <input type="date" className="form-control" id="birth" placeholder="Birth" />
-                                    </div>
-                                </div>
-                                <div className="row mt-4">
-                                    <div className="col">
-                                        <input type="email" className="form-control" id="email" placeholder="Email Address" />
-                                    </div>
-                                </div>
-                            </form>
-                            <div className="row d-flex align-items-end">
-                                <div className="col">
-                                    <div className="row p-2 mt-4">
-                                        <img src={icon_info} className="icon" alt="" />
-                                        <h5 className="d-flex my-auto">Please fill in the data correctly</h5>
-                                    </div>
-                                    <div className="row p-2">
-                                        <img src={icon_info} className="icon" alt="" />
-                                        <h5 className="d-flex my-auto">I agree to the applicable Terms and Conditions.</h5>
-                                    </div>
-                                </div>
-                                <div className="col-md-4 text-end p-0 mx-0">
-                                    <button className="btn btn-primary float-end">Sign Up</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            <div className="row">
+              <form className="form-register">
+                <div className="row">
+                  <div className="col">
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="fullname"
+                      placeholder="Fullname"
+                    />
+                  </div>
+                  <div className="col">
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="phone-number"
+                      placeholder="Phone Number"
+                    />
+                  </div>
                 </div>
+                <div className="row mt-4">
+                  <div className="col">
+                    <select id="inputState" className="form-select">
+                      <option selected>Gender</option>
+                      <option>L</option>
+                      <option>P</option>
+                    </select>
+                  </div>
+                  <div className="col">
+                    <input
+                      type="date"
+                      className="form-control"
+                      id="birth"
+                      placeholder="Birth"
+                    />
+                  </div>
+                </div>
+                <div className="row mt-4">
+                  <div className="col">
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      placeholder="Email Address"
+                    />
+                  </div>
+                </div>
+              </form>
+              <div className="row d-flex align-items-end">
+                <div className="col">
+                  <div className="row p-2 mt-4">
+                    <img src={icon_info} className="icon" alt="" />
+                    <h5 className="d-flex my-auto">
+                      Please fill in the data correctly
+                    </h5>
+                  </div>
+                  <div className="row p-2">
+                    <img src={icon_info} className="icon" alt="" />
+                    <h5 className="d-flex my-auto">
+                      I agree to the applicable Terms and Conditions.
+                    </h5>
+                  </div>
+                </div>
+                <div className="col-md-4 text-end p-0 mx-0">
+                  <button className="btn btn-primary float-end">Submit</button>
+                </div>
+              </div>
             </div>
-            <Faq />
-            <Footer />
+          </div>
         </div>
-    );
-}
+      </div>
+      <Faq />
+      <Footer />
+    </div>
+  );
+};
 
 export default Transaction;
-
