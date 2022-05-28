ff55c37843973302a0d7746530d2ded580be7bdd
Moh Ibnu Abdurrohman Sutio <ibnuabdurrohmansutio@gmail.com>
2022-05-28 19:54:24 +0700

    menampilkan data quiz pada admin dan fungsi tambah quiz

--- src/assets/pages/Dashboard Admin/DashboardAdmin.jsx
+++ src/assets/pages/Dashboard Admin/DashboardAdmin.jsx
@@ -7,6 +7,12 @@ import Header from "../Dashboard User/partials/Header";
 const DashboardAdmin = () => {
     const [conselors, setConselor] = useState([]);
     const [token, setToken] = useState(localStorage.getItem("token"));
+    const [quizzes, setQuizzes] = useState([]);
+
+    // form quiz field
+    const [title, setTitle] = useState("");
+    const [photo, setPhoto] = useState("");
+    const [description, setDescription] = useState("");
 
     useEffect(() => {
         axios.get('http://127.0.0.1:8000/api/users?role_id=2')
@@ -22,11 +28,38 @@ const DashboardAdmin = () => {
                 headers: {
                     Authorization: `Bearer ${token}`
                 }
-            }) 
+            })
+                .then(res => {
+                    swal("Deleted!", "Your file has been deleted.", "success");
+                    window.location.reload();
+                })
+        } catch (error) {
+            swal("Oops!", "Something went wrong!", "error");
+        }
+    }
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/api/quizzes')
             .then(res => {
-                swal("Deleted!", "Your file has been deleted.", "success");
+                setQuizzes(res.data.data);
+            });
+    }, []);
+
+    // add quiz
+    const addQuiz = async () => {
+        const formData = new FormData();
+        formData.append("title", title);
+        formData.append("photo", photo);
+        formData.append("description", description);
+        try {
+            await axios.post(`http://127.0.0.1:8000/api/quiz/store`, formData, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            swal("Success!", "Your file has been added.", "success").then(() => {
                 window.location.reload();
-            })           
+            });
         } catch (error) {
             swal("Oops!", "Something went wrong!", "error");
         }
@@ -91,24 +124,78 @@ const DashboardAdmin = () => {
                                         </h5>
                                     </div>
                                     <div className="col-md-3 text-end">
-                                        <a className="btn btn-primary" href="/add-diary">
+                                        <a className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addQuiz">
                                             Add New
                                         </a>
                                     </div>
                                 </div>
-                                <div className=" d-flex justify-content-around">
-                                    {/* <QuizItem
-                                        gambar="https://images.unsplash.com/photo-1581300134629-4c3a06a31948?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-                                        title="Make Me Happy" />
-                                    <QuizItem
-                                        gambar="https://images.unsplash.com/photo-1581300134629-4c3a06a31948?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-                                        title="Make Me Happy" />
-                                    <QuizItem
-                                        gambar="https://images.unsplash.com/photo-1581300134629-4c3a06a31948?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-                                        title="Make Me Happy" />
-                                    <QuizItem
-                                        gambar="https://images.unsplash.com/photo-1581300134629-4c3a06a31948?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-                                        title="Make Me Happy" /> */}
+                                <div className="d-flex justify-content-start">
+                                    {
+                                        quizzes.map((quiz) => {
+                                            return (
+                                                <div className=" col-md-2 col-sm-4 mt-4 me-5" key={quiz.id}>
+                                                    <div div className="card custom-card-course" >
+                                                        <img src={`http://127.0.0.1:8000/` + quiz.photo} className="card-img-top quiz-image" alt="..." />
+                                                        <div className="card-body conseling-body mt-3">
+                                                            <div className="row d-flex justify-item-center m-2">
+                                                                <div className="col-md d-flex justify-content-center">
+                                                                    <h5 className="card-title conselor-name giveMeEllipsis">{quiz.title}</h5>
+                                                                </div>
+                                                            </div>
+                                                            <div className="row d-flex justify-item-center mb-4 ">
+                                                                <div className="col-md d-flex justify-content-center">
+                                                                    <h6 className="text-secondary">{
+                                                                        quiz.created_at.split("T")[0].split("-").reverse().join("-")
+                                                                    }</h6>
+                                                                </div>
+                                                            </div>
+                                                            <div className="row d-flex align-items-center ps-2 mb-2">
+                                                                <div className="col-md d-flex justify-content-center">
+                                                                    <button className="btn btn-primary btn-small">Detail Quiz</button>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div >
+                                                </div>
+                                            );
+                                        })
+                                    }
+                                </div>
+
+                                {/* Modal Add Quiz */}
+                                <div className="modal fade" id="addQuiz" tabIndex="-1" aria-labelledby="addQuizLabel" aria-hidden="true">
+                                    <div className="modal-dialog">
+                                        <div className="modal-content">
+                                            <div className="modal-header">
+                                                <h5 className="modal-title" id="addQuizLabel">Add New Quiz</h5>
+                                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div className="modal-body">
+                                                <div className="mb-3">
+                                                    <label htmlFor="title" className="form-label">Title</label>
+                                                    <input type="email" className="form-control" id="title" onChange={(e) => {
+                                                        setTitle(e.target.value);
+                                                    }} placeholder="Quiz xxx" />
+                                                </div>
+                                                <div className="mb-3">
+                                                    <label htmlFor="photo" className="form-label">Photo</label>
+                                                    <input type="file" className="form-control" onChange={(e) => {
+                                                        setPhoto(e.target.files[0]);
+                                                    }} id="photo"/>
+                                                </div>
+                                                <div className="mb-3">
+                                                    <label htmlFor="description" className="form-label">Description</label>
+                                                    <textarea className="form-control" id="description" rows="3" onChange={(e) => {
+                                                        setDescription(e.target.value);
+                                                    }}></textarea>
+                                                </div>
+                                            </div>
+                                            <div className="modal-footer">
+                                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                <button type="button" className="btn btn-primary" onClick={addQuiz}>Add New</button>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                             <div
@@ -180,7 +267,7 @@ const DashboardAdmin = () => {
                                                                                     swal("Your imaginary file is safe!");
                                                                                 }
                                                                             }
-                                                                        );
+                                                                            );
                                                                     }}>Delete</a>
                                                                 </td>
                                                             </tr>
