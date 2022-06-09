41b75e7f57a128d9cde85c9d7d4327980185145e
Tharixs <tharixsakbar@gmail.com>
2022-06-09 20:53:10 +0700

    Update Login.jsx
    Co-Authored-By: E41200368_Nila Amalia_Gol A <74764135+nilamalia02@users.noreply.github.com>
    Co-Authored-By: NOGA MUKTIWATI <80508180+NOGAMUKTIWATI@users.noreply.github.com>
    Co-Authored-By: Alfian Ezza Raditya <rianezza86@gmail.com>
    Co-Authored-By: MOH IBNU ABDURROHMAN SUTIO <78123715+E41200036@users.noreply.github.com>

--- src/assets/pages/Login.jsx
+++ src/assets/pages/Login.jsx
@@ -42,11 +42,11 @@ const Login = () => {
       }),
     })
       .then((res) => res.json())
-      .then((data) => {
+      .then((res) => {
         // console.log(data);
-        if (data.meta.code == 200) {
-          localStorage.setItem("token", data.data.token);
-          if (data.data.user.role_id == 1) {
+        if (res.meta.code == 200) {
+          localStorage.setItem("token", res.data.token);
+          if (res.data.user.role_id == 1) {
             window.location.href = "/dashboard-user";
             setUser(res.user);
           } else if (res.data.user.role_id == 2) {
@@ -60,16 +60,16 @@ const Login = () => {
             } else {
               swal("Login Failed", "Kamu belum terverifikasi", "error")
             }
-            setUser(data.user);
-          } else if (data.data.user.role_id == 3) {
+            setUser(res.user);
+          } else if (res.data.user.role_id == 3) {
             // window.location.href = "/dashboard-admin";
             swal("Login Success", "Kamu adalah admin", "success").then(() => {
               window.location.href = '/dashboard-admin';
             });
-            setUser(data.user);
+            setUser(res.user);
           }
-        } else if (data.meta.code == 400) {
-          swal("Login Failed", data.data, "error");
+        } else if (res.meta.code == 400) {
+          swal("Login Failed", res.data, "error");
         }
 
         // resetForm();
