1ac6cf97aaea73361685b8566db635299f255746
Tharixs <tharixsakbar@gmail.com>
2022-06-16 14:34:59 +0700

    mengubah soal pada quiz menjadi dinamis
    Co-Authored-By: E41200368_Nila Amalia_Gol A <74764135+nilamalia02@users.noreply.github.com>
    Co-Authored-By: NOGA MUKTIWATI <80508180+NOGAMUKTIWATI@users.noreply.github.com>
    Co-Authored-By: MOH IBNU ABDURROHMAN SUTIO <78123715+E41200036@users.noreply.github.com>
    Co-Authored-By: Alfian Ezza Raditya <rianezza86@gmail.com>

--- src/assets/pages/StartQuiz.jsx
+++ src/assets/pages/StartQuiz.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import swal from "sweetalert";
 import Faq from "../../components/Faq";
 import Footer from "../../components/Footer";
@@ -8,9 +8,10 @@ import Header from "../../components/Header";
 import QuizItem from "../../items/QuizItem";
 
 const StartQuiz = () => {
-    const [quizzes, setQuizzes] = useState([]);
+    const [quizzes, setQuizzes] = useState({});
+    const { id } = useParams();
     useEffect(() => {
-        axios.get('http://127.0.0.1:8000/api/quizzes')
+        axios.get('http://127.0.0.1:8000/api/detail-quiz/' + id)
             .then(res => {
                 setQuizzes(res.data.data);
                 console.log(res.data.data);
@@ -18,84 +19,98 @@ const StartQuiz = () => {
             .catch(err => {
                 console.log(err);
             })
-    }, []);
+    }, [id]);
 
     return (
         <div>
             <Header />
             <div className="container">
-                {quizzes.map((quiz) => {
-                    return (
-                        <div className=" col-lg-12" key={quiz.id}>
-                            <div div className="card custom-card-course">
-                                <img src={`http://127.0.0.1:8000/${quiz.photo}`} className="card-img-top quiz-image" alt="..." />
-                                <div className="card-body conseling-body mt-3">
-                                    {
-                                        quiz.detail_quiz.map((detail, index) => {
-                                            return (
-                                                <div className="container col-lg-6">
-                                                    <div className="row d-flex justify-item-center mb-5 mt-5">
-                                                        <div className="col-md d-flex justify-content-start">
-                                                            <h5 className="card-title conselor-name giveMeEllipsis">{detail.title}</h5>
-                                                        </div>
-                                                        <div className="col-md d-flex justify-content-end">
-                                                            <h5 className="text-secondary">{index + 1}</h5>
-                                                        </div>
+                <div className="row justify-content-center">
+                    <div className=" col-lg-8" key={quizzes?.id}>
+                        <div div className="card custom-card-course" >
+                            <img src={`http://127.0.0.1:8000/${quizzes?.photo}`} className="card-img-top" style={
+                                {
+                                    width: '100%',
+                                    height: '400px',
+                                    objectFit: 'cover'
+                                }
+                            } alt="..." />
+                            <div className="card-body conseling-body mt-3">
+                                {
+                                    quizzes?.detail_quiz?.map((detail, index) => {
+                                        return (
+                                            <div className="container col-lg-12">
+                                                <div className="row d-flex justify-item-center mb-5 mt-5">
+                                                    <div className="col-md-10 d-flex justify-content-start">
+                                                        <h5 className="card-title conselor-name">{detail.title}</h5>
                                                     </div>
-                                                    <div className="row d-flex justify-content-center mb-2 ">
-                                                        <input type="radio" className="btn-check" name={`${index}`} id={`${index}, 1`} autocomplete="off" />
-                                                        <label className="btn btn-outline-success" for={`${index}, 1`}>{detail.question1}</label>
-                                                    </div>
-                                                    <div className="row d-flex justify-content-center mb-2 ">
-                                                        <input type="radio" className="btn-check" name={`${index}`} id={`${index}, 2`} autocomplete="off" />
-                                                        <label className="btn btn-outline-success" for={`${index}, 2`}>{detail.question2}</label>
-                                                    </div>
-                                                    <div className="row d-flex justify-content-center mb-2 ">
-                                                        <input type="radio" className="btn-check" name={`${index}`} id={`${index},3`} autocomplete="off" />
-                                                        <label className="btn btn-outline-success" for={`${index},3`}>{detail.question3}</label>
-                                                    </div>
-                                                    <div className="row d-flex justify-content-center mb-2 ">
-                                                        <input type="radio" className="btn-check" name={`${index}`} id={`${index}4`} autocomplete="off" />
-                                                        <label className="btn btn-outline-success" for={`${index}4`}>{detail.question4}</label>
+                                                    <div className="col-md d-flex justify-content-end">
+                                                        <h5 className="text-secondary">{index + 1}</h5>
                                                     </div>
                                                 </div>
-
-                                            )
-                                        })
-                                    }
-                                    <div className="row justify-content-center mt-5">
-                                        <div className="m-0 col-lg-6 text-end">
-                                            <button className="btn btn-primary me-0" onClick={
-                                                () => {
-                                                    swal({
-                                                        title: "Are you sure?",
-                                                        text: "Once submitted, you will not be able to edit your answers!",
-                                                        icon: "warning",
-                                                        buttons: true,
-                                                        dangerMode: true,
-                                                    })
-                                                        .then((willDelete) => {
-                                                            if (willDelete) {
-
-                                                                swal("Your answers has been submitted!", {
-                                                                    icon: "success",
-                                                                });
-                                                                window.location.href = `/courses`;
-                                                            } else {
-                                                                swal("Your answers has been canceled!");
-                                                            }
-                                                        });
+                                                {
+                                                    detail.question1 == '' || detail.question1 == 'undefined' || detail.question1 == null ? null :
+                                                        <div className="row d-flex justify-content-center mb-2 ">
+                                                            <input type="radio" className="btn-check" name={`${index}`} id={`${index}, 1`} autocomplete="off" />
+                                                            <label className="btn btn-outline-success" for={`${index}, 1`}>{detail.question1}</label>
+                                                        </div>
                                                 }
-                                            }>Submit</button>
-                                        </div>
+                                                {
+                                                    detail.question2 == '' || detail.question2 == 'undefined' || detail.question2 == null ? null :
+                                                        <div className="row d-flex justify-content-center mb-2 ">
+                                                            <input type="radio" className="btn-check" name={`${index}`} id={`${index}, 2`} autocomplete="off" />
+                                                            <label className="btn btn-outline-success" for={`${index}, 2`}>{detail.question2}</label>
+                                                        </div>
+                                                }
+                                                {
+                                                    detail.question3 == '' || detail.question3 == 'undefined' || detail.question3 == null ? null :
+                                                        <div className="row d-flex justify-content-center mb-3 ">
+                                                            <input type="radio" className="btn-check" name={`${index}`} id={`${index}, 3`} autocomplete="off" />
+                                                            <label className="btn btn-outline-success" for={`${index}, 3`}>{detail.question3}</label>
+                                                        </div>
+                                                }
+                                                {
+                                                    detail.question4 == '' || detail.question4 == 'undefined' || detail.question4 == null ? null :
+                                                        <div className="row d-flex justify-content-center mb-4 ">
+                                                            <input type="radio" className="btn-check" name={`${index}`} id={`${index}, 4`} autocomplete="off" />
+                                                            <label className="btn btn-outline-success" for={`${index}, 4`}>{detail.question4}</label>
+                                                        </div>
+                                                }
+                                            </div>
+
+                                        )
+                                    })
+                                }
+                                <div className="row justify-content-end mt-5 me-1 mb-2">
+                                    <div className="m-0 col-lg-6 text-end">
+                                        <button className="btn btn-success me-0" onClick={
+                                            () => {
+                                                swal({
+                                                    title: "Are you sure?",
+                                                    text: "Once submitted, you will not be able to edit your answers!",
+                                                    icon: "warning",
+                                                    buttons: true,
+                                                    dangerMode: true,
+                                                })
+                                                    .then((willDelete) => {
+                                                        if (willDelete) {
+
+                                                            swal("Your answers has been submitted!", {
+                                                                icon: "success",
+                                                            });
+                                                            window.location.href = `/courses`;
+                                                        } else {
+                                                            swal("Your answers has been canceled!");
+                                                        }
+                                                    });
+                                            }
+                                        }>Submit</button>
                                     </div>
                                 </div>
-
-                            </div >
+                            </div>
                         </div >
-                    );
-                })}
-
+                    </div >
+                </div>
             </div >
             <Faq />
             <Footer />
--- src/items/QuizItem.jsx
+++ src/items/QuizItem.jsx
@@ -5,7 +5,8 @@ const QuizItem = (props) => {
     return (
         <div className=" col-md-2 col-sm-4 mt-4">
             <div div className="card custom-card-course" >
-                <img src={`http://127.0.0.1:8000/` + props.gambar} className="card-img-top quiz-image" alt="..." />
+                <img src={`http://127.0.0.1:8000/` + props.gambar} className="card-img-top quiz-image"
+                    alt="..." />
                 <div className="card-body conseling-body mt-3">
                     <div className="row d-flex justify-item-center m-2">
                         <div className="col-md d-flex justify-content-center">
@@ -14,7 +15,7 @@ const QuizItem = (props) => {
                     </div>
                     <div className="row d-flex justify-item-center mb-4 ">
                         <div className="col-md d-flex justify-content-center">
-                            <h6 className="text-secondary">{props.description}</h6>
+                            <h6 className="text-secondary giveMeEllipsis">{props.description}</h6>
                         </div>
                     </div>
                     <div className="row d-flex align-items-center ps-2 mb-2">
